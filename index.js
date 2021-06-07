cf2ea31c3fc897ad75a353277a813bf70ce5e6e7
jopemachine <jopemachine@naver.com>
2021-06-08 01:46:56 +0900

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const arvish = require('@jopemachine/arvish');
+const lorem = require('lorem-ipsum');
+
+function generateSentenceOutput(count) {
+  return {
+    title: count > 1 ? `${count} Sentences` : 'Sentence',
+    subtitle: 'Press enter to copy',
+    arg: lorem({
+      units: 'sentences',
+      count,
+      sentenceLowerBound: 5,
+      sentenceUpperBound: 15
+    })
+  };
+}
+
+function generateParagraphOutput(count) {
+  return {
+    title: count > 1 ? `${count} Paragraphs` : 'Paragraph',
+    subtitle: 'Press enter to copy',
+    arg: lorem({
+      units: 'paragraphs',
+      count,
+      paragraphLowerBound: 5,
+      paragraphUpperBound: 15
+    })
+  };
+}
+
+const count = parseInt(arvish.input, 10) || 1;
+
+arvish.output([
+  generateParagraphOutput(count),
+  generateSentenceOutput(count)
+]);
---
+++ readme.md
@@ -0,0 +1,24 @@
+# arvis-loremipsum
+
+> Grab placeholder text in [Arvis](https://github.com/jopemachine/arvis)
+
+## Install
+
+```
+$ npm install --global arvis-loremipsum
+```
+
+*Requires [Node.js](https://nodejs.org) 4+.*
+
+
+## Usage
+
+In arvis, type `lorem`, <kbd>Enter</kbd>, and your query.
+
+Add an optional number to grab more than one paragraph or sentence.
+
+![Screenshot](screenshot.png)
+
+## License
+
+MIT © [Anton Niklasson](http://antonniklasson.se)
